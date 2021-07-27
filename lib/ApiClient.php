6b031fc6876a7b2d475402d2df2e4e0a6c8e8914
Johan Groenen <johan.groenen@gmail.com>
2021-07-27 21:23:15 +0200

    api backend

---
+++ lib/ApiClient.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tiltshift\Algoritmeregister;
+
+class ApiClient
+{
+    private $_apiUrl;
+
+    public function __construct($apiUrl)
+    {
+        $this->_apiUrl = $apiUrl;
+    }
+
+    public function readToepassingen()
+    {
+        $rs = json_decode(file_get_contents($this->_apiUrl . "/toepassingen"), true);
+        $toepassingen = $rs["_embedded"]["toepassingen"];
+        return $toepassingen;
+    }
+
+    public function createToepassing($data)
+    {
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => http_build_query($data)
+            ]
+        ]);
+        return json_decode(file_get_contents($this->_apiUrl . "/toepassingen", false, $context), true);
+    }
+
+    public function readToepassing($id)
+    {
+        return json_decode(file_get_contents($this->_apiUrl . "/toepassingen/{$id}"), true);
+    }
+
+    public function updateToepassing($id, $data)
+    {
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'PUT',
+                'header' => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => http_build_query($data)
+            ]
+        ]);
+        return json_decode(file_get_contents($this->_apiUrl . "/toepassingen/{$id}", false, $context), true);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Mailgun\Mailgun;
-use Tiltshift\Algoritmeregister\Algoritmeregister;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../private/config.php';
@@ -18,107 +17,99 @@ $twig = Twig::create(__DIR__ . '/../templates', [
 
 $app->add(TwigMiddleware::create($app, $twig));
 
-$algoritmeregister = new Algoritmeregister(__DIR__ . "/../storage/");
+$algoritmeregister = new \Tiltshift\Algoritmeregister\ApiClient($config["api-url"]);
 
 $app->get('/', function (Request $request, Response $response, $args) use ($algoritmeregister) {
     $view = Twig::fromRequest($request);
+    $toepassingen = $algoritmeregister->readToepassingen();
     return $view->render($response, 'overzicht.twig', [
-        'items' => $algoritmeregister->getIndex()
+        'items' => $toepassingen
     ]);
 });
 
 $app->get('/over', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'over.twig', [
-        'title' => 'Over Algoritmeregister'
+        'title' => 'Over het Algoritmeregister'
     ]);
 });
 
 $app->get('/aanmelden', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'aanmelden.twig', [
-        'title' => 'Algoritme aanmelden',
-        'description' => ''
+        'title' => 'Algoritmische Toepassing Aanmelden',
+        'description' => 'Meld je Algoritmische Toepassing aan.'
     ]);
 });
 
 $app->post('/aanmelden', function (Request $request, Response $response, $args) use ($config, $algoritmeregister) {
     $data = $request->getParsedBody();
-    $organisatie = $data["organisatie"];
-    $afdeling = $data["afdeling"];
-    $naam = $data["naam"];
-    $contact = $data["contact"];
-    $type = "onbekend";
-    $status = "aangemeld";
-    $herziening = date("d-m-Y");
 
+    $data["type"] = "onbekend";
+    $data["status"] = "aangemeld";
+    $data["herziening"] = date("d-m-Y");
+
+    $contact = $data["contact"];
+    
     $maildomain = array_pop(explode('@', $contact));
     if (!in_array($maildomain, $config['known-maildomains'])) {
         return $response->withHeader("Location", "/aanmelden")->withStatus(303);
     }
 
-    $algoritmeInfo = $algoritmeregister->create();
-    $algoritmeInfo->setNaam($naam);
-    $algoritmeregister->store($algoritmeInfo);
-    $algoritmeregister->storeIndex($uuid, $organisatie, $afdeling, $naam, $type, $status, $herziening, $contact, $hash);
+    $toepassing = $algoritmeregister->createToepassing($data);
+    
+    $naam = $toepassing["naam"]["waarde"];
+    $uuid = $toepassing["uuid"]["waarde"];
+    $token = $toepassing["token"]["waarde"];
 
     $mgClient = Mailgun::create($config["mailgun-key"], $config["mailgun-url"]);
     $result = $mgClient->messages()->send("algoritmeregister.nl", array(
         'from'	=> 'Algoritmeregister <no-reply@algoritmeregister.nl>',
         'to'	=> $contact,
-        'subject' => "Detailpagina {$naam} beschikbaar",
-        'text'	=> "Je bent aangemeld als beheerder voor de detailpagina van {$naam} in het Algoritmeregister. Op https://www.algoritmeregister.nl/details/{$uuid}?token={$token} kun je de gegevens bekijken en bijwerken."
+        'subject' => "Detailpagina toepassing \"{$naam}\" beschikbaar",
+        'text'	=> "Je bent aangemeld als beheerder voor de detailpagina van de toepassing \"{$naam}\" in het Algoritmeregister. Op https://www.algoritmeregister.nl/details/{$uuid}?token={$token} kun je de gegevens bekijken en bijwerken."
     ));
     
     return $response->withHeader("Location", "/details/{$uuid}")->withStatus(303);
 });
 
-$app->get('/details/{id}', function (Request $request, Response $response, $args) {
+$app->get('/details/{id}', function (Request $request, Response $response, $args) use ($algoritmeregister) {
     $view = Twig::fromRequest($request);
     $id = $args['id'];
-    $metadata = json_decode(file_get_contents(__DIR__ . "/../storage/{$id}." . md5($id) . ".json"), true);
+    $toepassing = $algoritmeregister->readToepassing($id);
     $grouped = [];
-    foreach ($metadata as $item) {
-        $grouped[$item["categorie"]][] = $item;
+    foreach ($toepassing as $field) {
+        if ($field["categorie"]) {
+            $grouped[$field["categorie"]][] = $field;
+        }
     }
     return $view->render($response, 'details.twig', [
         'id' => $id,
-        'title' => $metadata["naam"]["waarde"],
-        'description' => $metadata["beschrijving"]["waarde"],
+        'title' => $toepassing["naam"]["waarde"],
+        'description' => $toepassing["beschrijving"]["waarde"],
         'grouped' => $grouped
     ]);
 });
 
-$app->get('/data/{id}', function (Request $request, Response $response, $args) {
-    $id = $args['id'];
-    header("Content-type: text/json");
-    readfile(__DIR__ . "/../storage/{$id}." . md5($id) . ".json");
-    die;
-});
-
-$app->get('/aanpassen/{id}', function (Request $request, Response $response, $args) {
+$app->get('/aanpassen/{id}', function (Request $request, Response $response, $args) use ($algoritmeregister) {
     $view = Twig::fromRequest($request);
     $id = $args['id'];
-    $metadata = json_decode(file_get_contents(__DIR__ . "/../storage/{$id}." . md5($id) . ".json"), true);
+    $toepassing = $algoritmeregister->readToepassing($id);
     $grouped = [];
-    foreach ($metadata as $item) {
-        $grouped[$item["categorie"]][] = $item;
+    foreach ($toepassing as $field) {
+        $grouped[$field["categorie"]][] = $field;
     }
     return $view->render($response, 'aanpassen.twig', [
         'id' => $id,
-        'title' => $metadata["naam"]["waarde"],
-        'description' => $metadata["beschrijving"]["waarde"],
+        'title' => $toepassing["naam"]["waarde"],
+        'description' => $toepassing["beschrijving"]["waarde"],
         'grouped' => $grouped
     ]);
 });
 
-$app->post('/aanpassen/{id}', function (Request $request, Response $response, $args) {
+$app->post('/aanpassen/{id}', function (Request $request, Response $response, $args) use ($algoritmeregister) {
     $id = $args['id'];
-    $attributes = json_decode(file_get_contents(__DIR__ . "/../storage/{$id}." . md5($id) . ".json"), true);
-    foreach ($request->getParsedBody() as $key => $value) {
-        $attributes[$key]["waarde"] = $value;
-    }
-    file_put_contents(__DIR__ . "/../storage/{$id}." . md5($id) . ".json", json_encode($attributes));
+    $algoritmeregister->updateToepassing($id, $request->getParsedBody());
     return $response->withHeader("Location", "/details/{$id}")->withStatus(303);
 });
 
